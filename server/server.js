3967db86434bb6e0b9e43385e16de64f11a90a79
Anurag Mishra <2k24.cs1e.2414327@gmail.com>
2026-09-19 01:42:37 +0530

    Added Render backend configuration

--- server/server.js
+++ server/server.js
@@ -188,6 +188,8 @@ app.post("/api/run", async (req, res) => {
 
 });
 
-app.listen(5000, () => {
-  console.log("Omnivore backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Omnivore backend running on port ${PORT}`);
 });
